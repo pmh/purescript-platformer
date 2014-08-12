44f2ce077fa4433e11eab5b593fc445c9c8b81ee
Patrik Hedman <patrik.hedman@gmail.com>
2014-08-12 07:50:20 +0200

    add group function

--- src/Data/Phaser/Group.purs
+++ src/Data/Phaser/Group.purs
@@ -1,3 +1,10 @@
 module Group where
 
+import Control.Monad.Eff
+import Control.Monad.Eff.Phaser
+import Data.Foreign.EasyFFI
+
 foreign import data Group :: *
+
+group :: Game -> Eff (phaser :: Phaser) Group
+group = unsafeForeignFunction ["game", ""] "return game.add.group();"
