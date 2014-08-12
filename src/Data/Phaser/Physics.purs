5fc1b5a9504c13da10111b3d6916713ff48fa0ea
Patrik Hedman <patrik.hedman@gmail.com>
2014-08-12 07:33:27 +0200

    add physics module

---
+++ src/Data/Phaser/Physics.purs
@@ -0,0 +1,17 @@
+module Data.Phaser.Physics (PhysicsType(..), startSystem) where
+
+import Control.Monad.Eff
+import Control.Monad.Eff.Phaser
+import Data.Foreign.EasyFFI
+
+data PhysicsType = Arcade | Chipmunk | Ninja | P2JS | Box2D
+
+startSystem' :: forall eff. Game -> Number -> Eff (phaser :: Phaser | eff) Unit
+startSystem' = unsafeForeignFunction ["game", "type", ""] "game.physics.startSystem(type);"
+
+startSystem :: forall eff. Game -> PhysicsType -> Eff (phaser :: Phaser | eff) Unit
+startSystem game Arcade   = startSystem' game 0
+startSystem game P2JS     = startSystem' game 1
+startSystem game Ninja    = startSystem' game 2
+startSystem game Box2D    = startSystem' game 3
+startSystem game Chipmunk = startSystem' game 5
